adba6aa032b4483d338853cbf0430d4988b8f069
ahmedIsti73 <113531290+ahmedIsti73@users.noreply.github.com>
2023-10-06 22:23:09 +0600

    Add files via upload

---
+++ Print_longest_Common_subsequence.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0);
+void file(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+#define endl '\n'
+#define ll long long int
+#define mem(arr,x) memset(arr,x,sizeof arr)
+//:::::::::::::::::::::::::::::::::::::::GlitcH::::::::::::::::::::::::::::::::
+string s1,s2;
+ll dp[4000][4000];
+int32_t main()
+{
+    file();
+    optimize();
+    cin>>s1>>s2;
+    ll x=s1.size(),y=s2.size();
+    for(int i=1;i<=x;i++){
+        for(int j=1;j<=y;j++){
+            if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    string s="";
+    while(x>0 && y>0){
+        if(s1[x-1]==s2[y-1]){
+            s+=s1[x-1];
+            x--,y--;
+        }else{
+            if(dp[x-1][y]>dp[x][y-1]){
+                x--;
+            }else y--;
+        }
+    }
+    reverse(s.begin(),s.end());
+    cout<<s<<endl;
+} 
