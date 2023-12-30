edb637d32108dbaf80128e426c718e7add95afc5
Istiaque Ahmed <113531290+ahmedIsti73@users.noreply.github.com>
2023-12-30 14:56:59 +0600

    Add files via upload

---
+++ nCr(template).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll mod=1e9+7;
+ll fact[69];
+ll poW(ll x, ll n)
+{
+    ll result = 1;
+    while (n > 0)
+    {
+        if (n & 1LL == 1)
+        {
+            result = (result * x)%mod;
+        }
+        x = (x * x)%mod;
+        n = n >> 1LL;
+    }
+    return result%mod;
+}
+ll nCr(ll n,ll r){
+    return (fact[n] * poW((fact[r]*fact[n-r])%mod,mod-2)) % mod;
+}
+int32_t main(){
+    fact[0]=1;
+    for(int i=1;i<=60;i++){
+        fact[i]=(fact[i-1]*i*1LL)%mod;
+    }
+    cout<<nCr(20,7)<<endl;
+}
