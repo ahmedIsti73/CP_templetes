edf93681adfa444b19d18eb4386f3e45ba9dc8fd
Istiaque Ahmed <113531290+ahmedIsti73@users.noreply.github.com>
2024-01-24 19:56:39 +0600

    Update Trie_Maximum_xor_of_an_array.cpp

--- Trie_Maximum_xor_of_an_array.cpp
+++ Trie_Maximum_xor_of_an_array.cpp
@@ -1,9 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define optimize()                 \
-    ios_base ::sync_with_stdio(0); \
-    cin.tie(0);                    \
-    cout.tie(0);
 #define ll long long int
 class trieNode
 {
@@ -11,24 +7,18 @@ public:
     trieNode *zero;
     trieNode *one;
 };
-class xor_trie
-{
+class xor_trie{
 public:
-    void insert(trieNode *root, ll n)
-    {
+    void insert(trieNode *root, ll n){
         trieNode *cur = root;
-        for (int i = 31; i >= 0; i--)
-        {
-            if ((n & (1LL << i)))
-            {
-                if (cur->one == NULL)
-                {
+        for (int i = 31; i >= 0; i--){
+            if ((n & (1LL << i))){
+                if (cur->one == NULL){
                     cur->one = new trieNode();
                 }
                 cur = cur->one;
             }
-            else
-            {
+            else{
                 if (cur->zero == NULL)
                 {
                     cur->zero = new trieNode();
@@ -37,26 +27,20 @@ public:
             }
         }
     }
-    ll findMaxXor(trieNode *root, ll n)
-    {
+    ll findMaxXor(trieNode *root, ll n){
         trieNode *cur = root;
         ll ans = 0;
-        for (int i = 31; i >= 0; i--)
-        {
-            if ((n & (1LL << i)))
-            {
-                if (cur->zero != NULL)
-                {
+        for (int i = 31; i >= 0; i--){
+            if ((n & (1LL << i))){
+                if (cur->zero != NULL){
                     ans += (1LL << i);
                     cur = cur->zero;
                 }
                 else
                     cur = cur->one;
             }
-            else
-            {
-                if (cur->one != NULL)
-                {
+            else{
+                if (cur->one != NULL){
                     ans += (1LL << i);
                     cur = cur->one;
                 }
@@ -66,26 +50,19 @@ public:
         }
         return ans;
     }
-    ll maxXor(vector<ll> &v, ll n)
-    {
+    ll maxXor(vector<ll> &v, ll n){
         trieNode *root = new trieNode();
         ll ans = 0;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
             insert(root, v[i]);
         }
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
             ans = max(ans, findMaxXor(root, v[i]));
         }
         return ans;
     }
 };
-int32_t main()
-{
-
-    optimize();
-
+int32_t main(){
     ll n;
     cin >> n;
     vector<ll> v(n);
