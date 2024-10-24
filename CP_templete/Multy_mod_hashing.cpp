16123ef0e7d559c4cead5dbaae0334d489d2e056
Istiaque Ahmed <113531290+ahmedIsti73@users.noreply.github.com>
2024-10-24 17:51:45 +0600

    Update Multy_mod_hashing.cpp

--- CP_templete/Multy_mod_hashing.cpp
+++ CP_templete/Multy_mod_hashing.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define ll long long int
 #define F               first
 #define S               second
-const ll p=65;
+const ll p=137;
 const ll N=1e6+10;
 const pair<ll,ll> mod={127657753,987654319};
 // ---------------------random mod generatior ------------------------
@@ -44,7 +44,7 @@ ll substract(ll a,ll b,ll mod){return ((a%mod)-(b%mod)+mod)%mod;}
 ll mult(ll a,ll b,ll mod) {return ((a%mod)*(b%mod))%mod;}
 ll fn(char ch){if(islower(ch)) return ch-'a'+1;if(isupper(ch)) return ch-'A'+1;return ch-'0'+1;}
 
-pair<ll,ll> pw[N+10],h[N+10],inv[N+10];
+pair<ll,ll> pw[N+10],inv[N+10];
 void precal(){
 	pw[0].F=pw[0].S=1;
 	for(int i=1;i<N;i++){
@@ -59,66 +59,40 @@ void precal(){
 		inv[i].S=mult(inv[i-1].S,pw_inv2,mod.S);
 	}	
 }
-void build(string s){
-	ll n=s.size();
-	for(int i=0;i<n;i++){
-		ll a1=0,b1,a2=0,b2;
-		if(i){
-			a1=h[i-1].F;
-			a2=h[i-1].S;
-		}
-		b1=mult(pw[i].F,fn(s[i]),mod.F);
-		b2=mult(pw[i].S,fn(s[i]),mod.S);
-		h[i].F=add(a1,b1,mod.F);
-		h[i].S=add(a2,b2,mod.S);
-	}
-}
-pair<ll,ll> gethash(ll l,ll r){
-	ll res1=h[r].F;
-	ll res2=h[r].S;
-	if(l){
-		res1=substract(res1,h[l-1].F,mod.F);
-		res2=substract(res2,h[l-1].S,mod.S);
-	}
-	res1 =mult(res1,inv[l].F,mod.F);
-	res2 =mult(res2,inv[l].S,mod.S);
-	return {res1,res2};
-}
-
+struct hashing {
+    pair<ll,ll> h[N+2];
+    void build(string s){
+        ll n=s.size();
+        for(int i=0;i<n;i++){
+            ll a1=0,b1,a2=0,b2;
+            if(i){
+                a1=h[i-1].F;
+                a2=h[i-1].S;
+            }
+            b1=mult(pw[i].F,fn(s[i]),mod.F);
+            b2=mult(pw[i].S,fn(s[i]),mod.S);
+            h[i].F=add(a1,b1,mod.F);
+            h[i].S=add(a2,b2,mod.S);
+        }
+    }
+    pair<ll,ll> gethash(ll l,ll r){
+        ll res1=h[r].F;
+        ll res2=h[r].S;
+        if(l){
+            res1=substract(res1,h[l-1].F,mod.F);
+            res2=substract(res2,h[l-1].S,mod.S);
+        }
+        res1 =mult(res1,inv[l].F,mod.F);
+        res2 =mult(res2,inv[l].S,mod.S);
+        return {res1,res2};
+    }
+}a;
 int main(){
 	optimize();
 	// randommod();
 	precal();
-
-    string s,t;
-	cin>>s>>t;
-	ll k; cin>>k;
-	ll n=s.size(),m=t.size();
-	ll mp[28]={};
-	for(int i=0;i<m;i++){
-		if(t[i]=='0'){
-			mp[i]=1;
-		}
-	}
-	ll pre[n+2]={};
-	if(mp[s[0]-'a']==1) pre[0]=1;
-	for(int i=1;i<n;i++){
-		pre[i]=pre[i-1]+mp[s[i]-'a'];
-	}
-	// for(int i=0;i<n;i++) cout<<pre[i]<<' '; cout<<endl;
-	build(s);
-	set<pair<ll,ll>>temp;
-	ll cnt=0;
-	for(int i=0;i<n;i++){
-		for(int j=i;j<n;j++){
-			ll q=pre[j];
-			if(i)q-=pre[i-1];
-			if(q<=k){
-				pair<ll,ll> x=gethash(i,j);
-				// for(int a=j;a<=j+i-1;a++) cout<<s[a]; cout<<endl;
-				temp.insert(x);
-			}
-		}
-	}
-	cout<<temp.size()<<endl;
+    string s,tem; cin>>s;
+    tem=s;
+    ll n=s.size();
+    a.build(s);
 }
