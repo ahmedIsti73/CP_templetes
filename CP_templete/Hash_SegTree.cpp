f774c4e4f496ab5077702d58a214c89cdff21229
Istiaque Ahmed <113531290+ahmedIsti73@users.noreply.github.com>
2025-10-10 01:24:54 +0600

    Update Hash_SegTree.cpp

--- CP_templete/Hash_SegTree.cpp
+++ CP_templete/Hash_SegTree.cpp
@@ -1,16 +1,11 @@
-#include<bits/stdc++.h>
+#include"bits/stdc++.h"
 using namespace std;
-#define optimize()                 \
-    ios_base ::sync_with_stdio(0); \
-    cin.tie(0);                    \
-    cout.tie(0);
-#define endl '\n'
-#define ll long long int
-#define F               first
-#define S               second
-const ll p=137;
-const ll N=2e5+10;
+using ll = long long; using pll = pair<ll,ll>;
+#define F   first
+#define S   second
+const ll p=137;     const ll N=2e5+10; // check range
 const pair<ll,ll> mod={127657753,987654319};
+
 ll powerr(ll a,ll b,ll mod){
 	ll r=1;
 	while(b){
@@ -24,8 +19,9 @@ ll add(ll a,ll b,ll mod){return ((a%mod)+(b%mod)+mod)%mod;}
 ll substract(ll a,ll b,ll mod){return ((a%mod)-(b%mod)+mod)%mod;}
 ll mult(ll a,ll b,ll mod) {return ((a%mod)*(b%mod))%mod;}
 ll fn(char ch){if(islower(ch)) return ch-'a'+1;if(isupper(ch)) return ch-'A'+1;return ch-'0'+1;}
+// ll fn(ll a[i]) return a[i]; //for integer hash
 
-pair<ll,ll> pw[N+10],inv[N+10];
+pair<ll,ll> pw[N+10],inv[N+10],inv_p_minus1;
 void precal(){
 	pw[0].F=pw[0].S=1;
 	for(int i=1;i<N;i++){
@@ -39,16 +35,39 @@ void precal(){
 		inv[i].F=mult(inv[i-1].F,pw_inv1,mod.F);
 		inv[i].S=mult(inv[i-1].S,pw_inv2,mod.S);
 	}	
+    inv_p_minus1 = {
+        powerr(p-1, mod.F-2, mod.F),
+        powerr(p-1, mod.S-2, mod.S)
+    };
 }
 struct hashing {
     vector<pair<ll,ll>> t;
-    string s;
+    vector<char>lazy; // lazy of integer for integer hash 
+    string s; // integer hash make vector<ll> a
     hashing(){}
     hashing(string _s){
         s=_s;
         ll n=s.size();
         t.resize(n*4);
-    } 
+        lazy.resize(n*4,'?');
+    }
+    inline void push(int node,int l,int r){
+        if(lazy[node]=='?') return;
+        ll len=(r-l+1);
+        ll sum1 = mult(mult(substract(pw[len].F, 1, mod.F), inv_p_minus1.F, mod.F), pw[l].F, mod.F);
+        ll sum2 = mult(mult(substract(pw[len].S, 1, mod.S), inv_p_minus1.S, mod.S), pw[l].S, mod.S);
+
+        t[node].F = mult(sum1, fn(lazy[node]), mod.F);
+        t[node].S = mult(sum2, fn(lazy[node]), mod.S);
+        if(l!=r){
+            lazy[node*2]=lazy[node*2+1]=lazy[node];
+        }
+        lazy[node]='?';
+    }
+    inline void here(int node){
+        t[node].F=add(t[node*2].F,t[node*2+1].F,mod.F);
+        t[node].S=add(t[node*2].S,t[node*2+1].S,mod.S);
+    }
     void build(int node,int l,int r){
         if(l==r){
             t[node].F=mult(pw[l].F,fn(s[l]),mod.F);
@@ -58,23 +77,23 @@ struct hashing {
         ll mid=(l+r)>>1;
         build(node*2,l,mid);
         build(node*2+1,mid+1,r);
-        t[node].F=add(t[node*2].F,t[node*2+1].F,mod.F);
-        t[node].S=add(t[node*2].S,t[node*2+1].S,mod.S);
+        here(node);
     }
-    void upd(int node,int l,int r,int i,char value){
-        if(l>i || r<i) return;
-        if(l==i && r==i){
-            t[node].F=mult(pw[i].F,fn(value),mod.F);
-            t[node].S=mult(pw[i].S,fn(value),mod.S);
+    void upd(int node,int l,int r,int i,int j,char value){
+        push(node,l,r);
+        if(l>j || r<i) return;
+        if(i<=l && r<=j){
+            lazy[node]=value;
+            push(node,l,r);
             return;
         }
         ll mid=(l+r)>>1;
-        upd(node*2,l,mid,i,value);
-        upd(node*2+1,mid+1,r,i,value);
-        t[node].F=add(t[node*2].F,t[node*2+1].F,mod.F);
-        t[node].S=add(t[node*2].S,t[node*2+1].S,mod.S);
+        upd(node*2,l,mid,i,j,value);
+        upd(node*2+1,mid+1,r,i,j,value);
+        here(node);
     }
     pair<ll,ll> query(int node,int l,int r,int i,int j){
+        push(node,l,r);
         if(l>j || r<i) return {0,0};           /// check here
         if(i<=l && r<=j) return t[node];
         ll mid=(l+r)>>1;
@@ -88,30 +107,24 @@ struct hashing {
         ck.S=mult(ck.S,inv[l].S,mod.S);
         return ck;
     }
-}a,b;
+}a;
 int main(){
-	optimize();
-	// randommod();
+    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	precal();
-    ll n,q; cin>>n>>q;
+    ll n,m,x; cin>>n>>m>>x;
+    ll q=m+x;
     string s; cin>>s;
     a = hashing(s);
     a.build(1,0,n-1);
-    reverse(s.begin(),s.end());
-    b= hashing(s);
-    b.build(1,0,n-1);
     while(q--){
         ll i; cin>>i;
         if(i==1){
-            ll ind; char c; cin>>ind>>c;
-            --ind;
-            a.upd(1,0,n-1,ind,c);
-            b.upd(1,0,n-1,n-ind-1,c);
+            ll l,r; char c; cin>>l>>r>>c; l--,r--;
+            a.upd(1,0,n-1,l,r,c);
         }else{
-            ll l,r; cin>>l>>r;
+            ll l,r,d; cin>>l>>r>>d;
             --l,--r;
-            if(a.get_hash(l,r,n)==b.get_hash(n-r-1,n-l-1,n))
-            cout<<"YES"<<endl;
+            if(d==(r-l+1) || a.get_hash(l,r-d,n)==a.get_hash(l+d,r,n)) cout<<"YES"<<endl;
             else cout<<"NO"<<endl;
         }
     }
