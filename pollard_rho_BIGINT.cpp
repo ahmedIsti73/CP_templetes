8fc6473d78bee14b997b03fdbc1b32c2c4848ab1
Istiaque Ahmed <113531290+ahmedIsti73@users.noreply.github.com>
2025-11-08 21:58:38 +0600

    Create pollard_rho_BIGINT.cpp

---
+++ pollard_rho_BIGINT.cpp
@@ -0,0 +1,180 @@
+// this code is for prime fact for gteatter tham 18 digit less then 26 digit approx
+#include "bits/stdc++.h"
+using namespace std;
+#define endl '\n'
+#define PB push_back
+using ll=__int128;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll rand(ll n) {
+    ll r=((ll)rng()<<64) | rng();
+    return r%(n-2)+1;
+}
+ll modMul(ll a,ll b,ll mod) {
+    ll res=0;
+    a%=mod;
+    while(b>0) {
+        if(b&1) {
+            res=res+a;
+            if(res>=mod) res-=mod;
+        }
+        a=a+a;
+        if(a>=mod) a-=mod;
+        b>>=1;
+    }
+    return res;
+}
+ll modPower(ll base,ll exp,ll mod) {
+    ll res=1;
+    base%=mod;
+    while(exp>0) {
+        if(exp%2==1) res=modMul(res,base,mod);
+        base=modMul(base,base,mod);
+        exp/=2;
+    }
+    return res;
+}
+ll gcd(ll a,ll b) {
+    while(b) {
+        a%=b;
+        swap(a,b);
+    }
+    return a;
+}
+const int MAX_SIEVE=1000001;
+vector<int> spf(MAX_SIEVE);
+void init_sieve() {
+    vector<int> primes;
+    for(int i=2;i<MAX_SIEVE;++i) {
+        if(!spf[i]) {
+            spf[i]=i;
+            primes.PB(i);
+        }
+        for(int p:primes) {
+            if(i*(ll)p>=MAX_SIEVE) break;
+            spf[i*p]=p;
+            if(!(i%p)) break;
+        }
+    }
+}
+bool MillerRabin(ll n,ll a,ll d,int s) {
+    ll x=modPower(a,d,n);
+    if(x==1 || x==n-1) return true;
+    for(int r=1;r<s;r++) {
+        x=modMul(x,x,n);
+        if(x==1) return false;
+        if(x==n-1) return true;
+    }
+    return false;
+}
+bool isPrime(ll n) {
+    if(n<=1) return false;
+    if(n<MAX_SIEVE) return spf[n]==n;
+    if(n==2 || n==3) return true;
+    if(!(n%2)) return false;
+    ll d=n-1;
+    int s=0;
+    while(!(d%2)) {
+        d/=2;
+        s++;
+    }
+    vector<ll> witnesses={2,3,5,7,11,13,17,19,23,29,31,37};
+    for(ll a:witnesses) {
+        if(n==a) return true;
+        if(!(MillerRabin(n,a,d,s))) return false;
+    }
+    return true;
+}
+ll pollard_rho(ll n) {
+    auto f =[&](ll x,ll c) {
+        return (modMul(x,x,n)+c)%n;
+    };
+    ll c=rand(n);
+    ll tortoise=2,hare=2,d=1;
+    ll product=1;
+    const int BATCH_SIZE=128;
+    int count=0;
+    while(1) {
+        tortoise=f(tortoise,c);
+        hare=f(f(hare,c),c);
+        if(tortoise==hare) {
+            c=rand(n);
+            tortoise=2; hare=2; product=1; count=0;
+            continue;
+        }
+        ll prev_product=product,diff;
+        if(tortoise>hare) diff=tortoise-hare;
+        else diff=hare-tortoise;
+        product=modMul(product,diff,n);
+        if(!product) {
+            d=gcd(prev_product,n);
+            if(d==1) d=gcd(diff,n);
+            break;
+        }
+        count++;
+        if(count==BATCH_SIZE) {
+            d=gcd(product,n);
+            if(d>1) break;
+            count=0;
+            product=1;
+        }
+    }
+    if(d==n || d==1) return pollard_rho(n);
+    return d;
+}
+void factorize(ll n,vector<ll>& primeFactors) {
+    if(n<=1) return;
+    while(!(n%2)) {
+        primeFactors.PB(2);
+        n/=2;
+    }
+    if(n==1) return;
+    while(n>1 && n<MAX_SIEVE) {
+        primeFactors.PB(spf[n]);
+        n/=spf[n];
+    }
+    if(n==1) return;
+    if(isPrime(n)) {
+        primeFactors.PB(n);
+        return;
+    }
+    ll d=pollard_rho(n);
+    factorize(d,primeFactors);
+    factorize(n/d,primeFactors);
+}
+ll read_u128() {
+    string s;
+    if(!(cin>>s)) return 0;
+    if(s=="0") return 0;
+    ll res=0;
+    for(auto c:s) res=res*10+(c-'0');
+    return res;
+}
+void print_u128(ll n) {
+    if(!n) {
+        cout<<0;
+        return;
+    }
+    string s;
+    while(n>0) {
+        s+=(char)('0'+(n%10));
+        n/=10;
+    }
+    reverse(s.begin(),s.end());
+    cout<<s;
+}
+void GLITCH_() {
+    init_sieve(); // run it before testcase
+    ll n; n=read_u128();
+    vector<ll> ans;
+    factorize(n,ans);
+    sort(ans.begin(),ans.end());
+    cout<<ans.size()<<' ';
+    for(auto u:ans){
+        print_u128(u);
+        cout<<' ';
+    }cout<<endl;
+}
+int32_t main() {
+    int t=1;
+    while(t--) GLITCH_();
+}
